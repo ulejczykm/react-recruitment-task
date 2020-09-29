e2f24a0d46aee3388dd9d94a8ddb2d025ec59909
Mariusz <ulejczykm@gmail.com>
2020-09-29 02:01:19 +0200

    Updates

---
+++ precache-manifest.9560cdb319e3c59ec12a738e15091481.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c6a33b719b92a6b8dc905c2eb5b096",
+    "url": "/react-recruitment-task/index.html"
+  },
+  {
+    "revision": "6be9fe84f237e1554192",
+    "url": "/react-recruitment-task/static/css/main.7f4d4014.chunk.css"
+  },
+  {
+    "revision": "679f281a20420279980f",
+    "url": "/react-recruitment-task/static/js/2.30f0c2a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-recruitment-task/static/js/2.30f0c2a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6be9fe84f237e1554192",
+    "url": "/react-recruitment-task/static/js/main.b042fb4f.chunk.js"
+  },
+  {
+    "revision": "c1cf3603f7e6e212b17f",
+    "url": "/react-recruitment-task/static/js/runtime-main.347b2de2.js"
+  }
+]);
